c3e5b7b292bfa89beae79118181fa369adbee429
HuiLs <dhqks507@gmail.com>
2023-09-08 14:27:46 +0900

    Bug Fixed

--- src/main/kotlin/com/example/bongocat/BongoCatToolWindow.kt
+++ src/main/kotlin/com/example/bongocat/BongoCatToolWindow.kt
@@ -4,10 +4,9 @@ import com.intellij.openapi.components.ServiceManager
 import com.intellij.openapi.editor.EditorFactory
 import com.intellij.openapi.editor.event.DocumentEvent
 import com.intellij.openapi.editor.event.DocumentListener
-import com.intellij.openapi.editor.event.EditorFactoryListener
-import com.intellij.openapi.fileEditor.FileDocumentManager
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.fileEditor.FileEditorManagerListener
+import com.intellij.openapi.fileEditor.ex.FileEditorWithProvider
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 import com.intellij.openapi.wm.ToolWindow
@@ -20,17 +19,19 @@ import javax.swing.JLabel
 import javax.swing.JPanel
 import javax.swing.Timer
 
-
-class BongoCatToolWindow : ToolWindowFactory, EditorFactoryListener,FileEditorManagerListener {
+class BongoCatToolWindow : ToolWindowFactory,FileEditorManagerListener {
     private val bongoLeft = ImageIcon(ImageIO.read(javaClass.classLoader.getResource("BongoCat_img/bongo_left.png")))
     private val bongoRight = ImageIcon(ImageIO.read(javaClass.classLoader.getResource("BongoCat_img/bongo_right.png")))
-    private val bongoMiddle = ImageIcon(ImageIO.read(javaClass.classLoader.getResource("BongoCat_img/bongo_middle.png")))
+    private val bongoMiddle =
+        ImageIcon(ImageIO.read(javaClass.classLoader.getResource("BongoCat_img/bongo_middle.png")))
+
     // 키 입력 시간을 저장하는 큐
     private val keyPressTimes: LinkedList<Long> = LinkedList()
     // 모든 열려있는 에디터에 DocumentListener 추가
 
     private val idleTimer: Timer
     private val label: JLabel
+
     // 생성자
     init {
         label = JLabel()
@@ -43,28 +44,7 @@ class BongoCatToolWindow : ToolWindowFactory, EditorFactoryListener,FileEditorMa
         idleTimer.isRepeats = false
 
     }
-    //새로 추가되는 document에 대해서 documentListener를 추가
-    private fun activateBongoCatForFile(file: VirtualFile, project: Project) {
-        val document = FileDocumentManager.getInstance().getDocument(file)
-        if (document != null) {
-            document.addDocumentListener(documentListener)
-            // 특정 파일에 대한 DocumentListener를 추가
-        }
-        // 원하는 동작 수행
-    }
-    override fun fileOpened(source: FileEditorManager, file: VirtualFile) {
-        source.selectedTextEditor?.document?.addDocumentListener(documentListener)
-        // 새로운 파일에 대한 DocumentListener를 추가
-        if (file.isDirectory || !file.isWritable || !file.isValid || file.length == 0L) {
-            return
-        }
-        val editor = source.selectedTextEditor
-        if (editor != null && editor.project != null) {
-            // 파일이 열렸을 때 원하는 작업 수행
-            // 예: 특정 파일에 대한 BongoCat 동작을 활성화
-            activateBongoCatForFile(file, editor.project!!)
-        }
-    }
+
 
     // DocumentListener를 구현
     private val documentListener = object : DocumentListener {
@@ -76,7 +56,7 @@ class BongoCatToolWindow : ToolWindowFactory, EditorFactoryListener,FileEditorMa
             keyPressTimes.addLast(currentTime)
 
             // 100ms 이내의 키 입력만 유지
-            keyPressTimes.removeIf { it < currentTime - 100}
+            keyPressTimes.removeIf { it < currentTime - 100 }
 
             // 빠른 키 입력 감지
             if (keyPressTimes.size >= 1) {
@@ -84,6 +64,8 @@ class BongoCatToolWindow : ToolWindowFactory, EditorFactoryListener,FileEditorMa
             }
         }
     }
+
+
     // EditorFactoryListener의 메서드를 구현
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
         val panel = JPanel()
@@ -102,5 +84,15 @@ class BongoCatToolWindow : ToolWindowFactory, EditorFactoryListener,FileEditorMa
         for (editor in editorFactory.allEditors) {
             editor.document.addDocumentListener(documentListener)
         }
+        project.messageBus.connect().subscribe(FileEditorManagerListener.FILE_EDITOR_MANAGER, this)
     }
+
+    override fun fileOpenedSync(
+        source: FileEditorManager,
+        file: VirtualFile,
+        editorsWithProviders: MutableList<FileEditorWithProvider>
+    ) {
+        source.getSelectedTextEditor()?.document?.addDocumentListener(documentListener)
+    }
+
 }
